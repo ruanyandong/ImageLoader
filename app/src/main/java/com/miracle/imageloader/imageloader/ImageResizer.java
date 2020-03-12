4c7af87177348587be3c781d2e936e4b3d4f7c13
ruanyandong <ruanyandong@qutoutiao.net>
2020-03-12 17:55:53 +0800

    ImageLoader first commit

---
+++ app/src/main/java/com/miracle/imageloader/imageloader/ImageResizer.java
@@ -0,0 +1,25 @@
+package com.miracle.imageloader.imageloader;
+
+/**
+ * @author miracle
+ * @date 2020-03-12
+ * @email ruanyandongai@gmail.com
+ * @blog https://ruanyandong.github.io
+ */
+
+/**
+ * 进行图片的压缩功能
+ */
+public class ImageResizer {
+
+    private static final String TAG = "ImageResizer";
+
+    public ImageResizer(){
+
+    }
+
+
+    public int calculateInSampleSize(){
+        return 0;
+    }
+}
